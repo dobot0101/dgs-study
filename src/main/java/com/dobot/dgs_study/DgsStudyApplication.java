65d4c7078702a5f4b676be957ccdc78acc383916
dobot0101 <lhl890@naver.com>
2024-06-13 00:52:12 +0900

    first commit

---
+++ src/main/java/com/dobot/dgs_study/DgsStudyApplication.java
@@ -0,0 +1,13 @@
+package com.dobot.dgs_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgsStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgsStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dobot/dgs_study/DgsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dobot.dgs_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgsStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
